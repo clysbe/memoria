7c7ccb86daef16dcf1aeda93599215429dc04983
Yo <saren_7@hotmail.com>
2016-02-10 00:02:49 -0600

    2

--- practica1/practica1Tests/practica1.swift
+++ practica1/practica1Tests/practica1.swift
@@ -34,3 +34,26 @@ class practica1Tests: XCTestCase {
     }
     
 }
+
+
+var numero = 0...100
+
+for n in numero {
+    
+    if (n % 5 == 0){
+    print("# \(n) Bingo!!!")
+    }
+    
+    if(n % 2 == 0){
+    print("# \(n) par!!!")
+    }
+    
+    if(n % 2 != 0){
+    print("# \(n) impar!!!")
+    }
+    
+    if(n >= 30 && n <= 40){
+    print(" # \(n) Viva Swift!!!")
+    }
+    
+}
