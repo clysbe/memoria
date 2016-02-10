fa4db2bd1935a89c0cac852fc97c642e83464244
Yo <saren_7@hotmail.com>
2016-02-10 00:00:46 -0600

    1

---
+++ practica1/uno/uno/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uno
+//
+//  Created by Sarale Jeronimo on 09/02/16.
+//  Copyright © 2016 Sarale Jeronimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
